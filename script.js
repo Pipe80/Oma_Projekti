694ae0734e9bc65e2f8fa7cc105877df69fb6105
Pipe80 <pirjomure@hotmail.com>
2023-02-27 15:29:55 +0200

    update

--- script.js
+++ script.js
@@ -9,9 +9,21 @@
 
 //Tämä on edelleen Udemyn kurssia, oli tarkoitus tehdä omaa projektia, mutta
 //se saa vielä hetken odottaa.
-const secretNumber = Math.trunc(Math.random()*20)+1;
+let secretNumber = Math.trunc(Math.random()*20)+1;
 let score=20;
 
+
+document.querySelector('.again').addEventListener('click', function() {
+    score=20;
+    secretNumber = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent =score;
+    document.querySelector('.number').textContent ='?';
+    document.querySelector('.guess').value='';
+ 
+    document.querySelector('.body').style.backgroundColor='#222';
+    document.querySelector('.number').style.width='15rem';
+});
 
 document.querySelector('.check').addEventListener
 ('click', function(){
