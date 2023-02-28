5f6ef68c924d2d9c113c67303277d84a6207b221
Pipe80 <pirjomure@hotmail.com>
2023-02-28 14:55:49 +0200

    update pig game

---
+++ Possupeli/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//selecting elements
+let score0El= document.querySelector('#score--0');
+let score1El= document.qetElementById('score--1');
+let diceEl=document.querySelector('.dice');
+
+//starting conditions
+score0El.textContent=0;
+score1El.textContent=0;
+diceEl.classList.add('hidden');
+
