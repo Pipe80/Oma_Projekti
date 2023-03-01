a4e01d9202176e5bcace48461e21d8fd58cd41f4
Pipe80 <pirjomure@hotmail.com>
2023-03-01 12:31:52 +0200

    update

--- Possupeli/script.js
+++ Possupeli/script.js
@@ -1,26 +1,40 @@
 'use strict';
 
 //selecting elements
+const player0El=document.querySelector('.player--0');
+const player1El=document.querySelector('.player--1');
 const score0El= document.querySelector('#score--0');
 const score1El= document.querySelector('#score--1');
+const current0El=document.getElementById('current--0');
+const current1El=document.getElementById('current--1');
 // const score1El= document.qetElementById('score--1');
 const diceEl=document.querySelector('.dice');
 const btnNew=document.querySelector('.btn--new');
 const btnRoll=document.querySelector('.btn--roll');
 const btnHold=document.querySelector('.btn--hold');
 
+const switchPlayer=function() {
+    document.getElementById(`current--${activePlayer}`).textContent =0;
+    currentScore= 0;
+    activePlayer = activePlayer === 0 ? 1: 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
 //starting conditions
 score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
 
+const scores=[0,0];
 let currentScore=0;
+let activePlayer=0;
 //rolling dice condition
 
 btnRoll.addEventListener('click',function(){
     //1. Generate a random dice roll
     const dice= Math.trunc(Math.random() * 6)+1;
-    console.log(dice);
+    // console.log(dice);
     //2. Display dice
     diceEl.classList.remove('hidden');
     diceEl.src= `dice-${dice}.png`;
@@ -29,11 +43,26 @@ btnRoll.addEventListener('click',function(){
 if(dice !==1){
     //add dice to current score
 currentScore += dice;
+document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+// current0El.textContent = currentScore;//CHANGE LATER
 
 } else{
     //switch to next player
+switchPlayer();
 }
+});
 
+btnHold.addEventListener('click', function(){
 
+    //add current score to active player's score
+scores[activePlayer] += currentScore;
+// scores[1] = scores[1] + currentScore;
+document.getElementById(`score--${activePlayer}`).textContent= 
+scores[activePlayer];
+    //2.check if player's score is >= 100
 
+    //finish the game
+
+    //Switch player
+    switchPlayer();
 });
